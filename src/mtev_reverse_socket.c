8633c6dde66c4be17dfd4fa1d08af2b5bc969f26
Riley Berton <riley.berton@circonus.com>
2016-10-18 18:28:57 +0000

    call service_ctx_free before nulling it out

--- src/mtev_reverse_socket.c
+++ src/mtev_reverse_socket.c
@@ -789,6 +789,7 @@ mtev_reverse_socket_acceptor(eventer_t e, int mask, void *closure,
 socket_error:
     /* Exceptions cause us to simply snip the connection */
     /* since we need to free the ac under abnormal circumstances, we must disassociate the service_ctx_free */
+    ac->service_ctx_free(ac->service_ctx);
     ac->service_ctx_free = NULL;
     acceptor_closure_free(ac);
     /* now clean up the reverse socket directly ourselves */
